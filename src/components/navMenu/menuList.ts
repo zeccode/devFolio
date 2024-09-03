8f2c41b56e381f8ed1f24e86bd99898847e84f3e
Michel Araujo <michel.araujo.zeca@gmail.com>
2024-09-03 03:36:50 -0300

    component navigation menu at routes

---
+++ src/components/navMenu/menuList.ts
@@ -0,0 +1,20 @@
+type MenuListType = {
+  label: string;
+  url: string;
+}
+
+const MenuList: MenuListType[] = [{
+  label: 'Home',
+  url: '/home',
+}, {
+  label: 'Entrar',
+  url: '/login',
+}, {
+  label: 'Sobre',
+  url: '/about',
+}, {
+  label: 'Fale Conosco',
+  url: '/contact-us',
+}
+]
+export default MenuList
--- src/components/navMenu/navMenu.tsx
+++ src/components/navMenu/navMenu.tsx
@@ -1,8 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import MenuList from './menuList';
+
+type handleClickType = (e: React.MouseEvent) => void
 
 const NavMenu = () => {
+  const handleClick: handleClickType = (e) => {
+    e.preventDefault();
+    alert('Login')
+  }
   return (
     <nav>
-      meu menu
+      <ul>
+        {MenuList.map((menuItem) => {
+          return (<li><Link to={`${menuItem.url}`}>{menuItem.label}</Link></li>)
+        })}
+      </ul>
+      <button type="button" onClick={handleClick}>Entrar</button>
     </nav>
   )
 }
